c0e772f6946e72691582307aa3e3ebacd9fd0b90
jahirulalam42 <jahirulalam41@gmail.com>
2025-09-10 17:18:48 +0600

    Added responsive to index

--- scripts/output.js
+++ scripts/output.js
@@ -4,10 +4,10 @@ let email = url.searchParams.get("email");
 let github = url.searchParams.get("github");
 
 document.getElementById("name-ticket").innerText = username;
-document.getElementById("name").innerText = username ;
+document.getElementById("name").innerText = username + " !";
 document.getElementById("email").innerText = email;
 document.getElementById("email-ticket").innerText = email;
 document.getElementById("github-ticket").innerText = github;
-console.log('This is Current Url',url);
-console.log('Name, email and Github',username,email,github)
+console.log('This is Current Url', url);
+console.log('Name, email and Github', username, email, github)
 
