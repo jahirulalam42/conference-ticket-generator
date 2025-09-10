a17abedcfae12edadd2c4843482e29f077a8bb32
MD.Jahirul Alam Sarkar <jahirulalam41@gmail.com>
2025-09-11 01:19:15 +0600

    Added generator function

--- scripts/output.js
+++ scripts/output.js
@@ -3,11 +3,17 @@ let username = url.searchParams.get("name");
 let email = url.searchParams.get("email");
 let github = url.searchParams.get("github");
 
+let check = Math.floor(Math.random() * 100000);
+
 document.getElementById("name-ticket").innerText = username;
 document.getElementById("name").innerText = username + " !";
 document.getElementById("email").innerText = email;
 document.getElementById("email-ticket").innerText = email;
-document.getElementById("github-ticket").innerText = github;
+
+document.getElementById("generate").innerText = "#" + check;
+
+console.log('Check',check)
+
 console.log('This is Current Url', url);
 console.log('Name, email and Github', username, email, github)
 
